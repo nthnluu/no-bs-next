b2dc3b84ce6aaca94ecbb40222c3f39c9518d7a8
nathanluu <nate1299@me.com>
2020-10-13 16:18:49 -0700

    Added authentication pages and snazz'd up the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-      <div>
-        <h1 className="text-4xl font-bold">Hello</h1>
+      <div className="h-screen flex justify-center items-center px-12">
+          <div className="text-center">
+              <img src="/logos.png" className="h-32 lg:h-48 mx-auto"/>
+              <h1 className="text-4xl font-bold text-gray-900">No BS Next 🔥</h1>
+              <h2 className="text-2xl text-gray-700">The fast way to build secure, full-stack web apps.</h2>
+
+          </div>
+
 
       </div>
 
